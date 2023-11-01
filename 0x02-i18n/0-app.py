c59a3d9dcb3e6e60d4615e83057bff089d3933b2
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-01 11:10:37 +0100

    done

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template
+'''Basic flask app'''
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
+    '''hello world'''
     return render_template('0-index.html')
