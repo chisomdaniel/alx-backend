b6b8dbb240ad95c0770cedfb163563bfdffe9c0b
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-01 13:35:31 +0100

    2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Basic babel setup'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    '''config class for the flask app'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def hello_world():
+    '''hello world'''
+    return render_template('1-index.html')
