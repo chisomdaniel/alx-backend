93d484df42dc6cfa4925cd14a148b3b1a08b8f88
Chisom Daniel <chinwezechisom@gmail.com>
2023-11-17 11:07:00 +0100

    fixed

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,5 +18,6 @@ class BasicCache(BaseCaching):
         '''get data from the cache'''
         if key is None:
             return None
+        if key not in self.cache_data:
+            return None
         return self.cache_data.get(key, None)
-
